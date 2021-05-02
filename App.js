2b1d95bb3fd2a11660a4017d58513aa615f1a1cd
Sunil Kumar <neil001002@gmail.com>
2021-05-02 23:34:20 +0530

    added simple orange clickable button

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, Image, View, SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView, Button } from 'react-native';
 
 export default function App() {
   console.log("App executed");
 
-  const handlePress = () => console.log("Text Clicked");
-
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native.</Text>
-      <Image source={require('./assets/icon.png')} />
+      <Button 
+        color="orange"
+        title="Click me" onPress={() => console.log("Button clicked")}/>
     </SafeAreaView>
   );
 }
