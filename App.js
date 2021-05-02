981915e0e161afdc7418b1a1ab706e5dea2d5fd6
Sunil Kumar <neil001002@gmail.com>
2021-05-03 00:57:36 +0530

    simple Welcome screen

--- App.js
+++ App.js
@@ -1,31 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, SafeAreaView, Button, Alert } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  console.log("App executed");
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <Button 
-        color="orange"
-        title="Click me" 
-        onPress={() => 
-          // (only IOS at the moment)
-          // Alert.prompt("My title", "My message", text => console.log(text))
-          Alert.alert("My title", "My message", [
-          {text: "Yes", onPress: () => console.log("Yes")},
-          {text: "No", onPress: () => console.log("No")}
-        ])}/>
-    </SafeAreaView>
-  );
+  return <WelcomeScreen/>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
