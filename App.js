bda5f21d4868c8092dec6d86ae6a650d5749a6dc
Sunil Kumar <neil001002@gmail.com>
2021-05-02 22:26:08 +0530

    ! to .

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native!</Text>
+      <Text>Hello React Native.</Text>
       <Image source={require('./assets/icon.png')} />
     </SafeAreaView>
   );
