9a3f2cc07dc52e2e161d56abc50525a913117f92
Sunil Kumar <neil001002@gmail.com>
2021-05-03 14:09:52 +0530

    simple Welcome screen

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, Button, Alert } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
 
-  return <WelcomeScreen/>;
+  return <ViewImageScreen />;
 }
 
